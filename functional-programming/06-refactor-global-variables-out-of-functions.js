122cc934b349e7edda2d8e1053d0a84aecfcb4ca
petya <sashafem@mail.ru>
2021-12-13 10:13:23 +0300

    Started Functional Programming

---
+++ functional-programming/06-refactor-global-variables-out-of-functions.js
@@ -0,0 +1,30 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add (arr, bookName) {
+  let newArr = arr.slice(0)
+  newArr.push(bookName);
+  return newArr;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove (arr, bookName) {
+  let newArr = arr.slice(0)
+  const book_index = newArr.indexOf(bookName);
+  if (book_index >= 0) {
+
+    newArr.splice(book_index, 1);
+    return newArr;
+
+    // Change code above this line
+    }
+}
+
+const newBookList = add(bookList, 'A Brief History of Time');
+const newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+const newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
